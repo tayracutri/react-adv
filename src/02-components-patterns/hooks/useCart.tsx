42042a6d90e7ff492d444ab2c1535ee17a482ba4
tayracutri10@gmail.com <tayracutri@gmail.com>
2023-04-04 15:35:10 -0300

    control props alternative

--- src/02-components-patterns/hooks/useCart.tsx
+++ src/02-components-patterns/hooks/useCart.tsx
@@ -11,37 +11,18 @@ export const useCart = () => {
     count: number;
     product: Product;
   }) => {
-    // console.log("onProductchange", count, product);
-
     console.log({ count });
     setCart((oldCart) => {
-      const productInCart: ProductInCart = oldCart[product.id] || {
-        ...product,
-        count: 0,
-      };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldCart;
 
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-
-        return {
-          ...oldCart,
-          [product.id]: productInCart,
-        };
+        return rest;
       }
-      //borrar el product si no existe o es menos a 0
-      const { [product.id]: toDelete, ...rest } = oldCart;
-      console.log(toDelete);
-      return rest;
-      // if (count === 0) {
-      // const { [product.id]: toDelete, ...rest } = oldCart;
-      // console.log(toDelete);
-      // return rest;
-      // }
 
-      // return {
-      //   ...oldCart,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...oldCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
